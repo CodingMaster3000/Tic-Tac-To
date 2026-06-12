a81ab06d96d06535f808b303545bef2f7989bcf2
CodingMaster3000 <67920617+CodingMaster3000@users.noreply.github.com>
2026-06-12 21:04:41 +0200

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Tic_Tac_To
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
