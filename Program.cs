0e41cc3f6effbeef35cbfc2926450bd76105f7b3
CodingMaster3000 <67920617+CodingMaster3000@users.noreply.github.com>
2026-06-20 20:57:51 +0200

    added user input interpretation
    added code that reads user input and marks the d escribed cell

--- Program.cs
+++ Program.cs
@@ -5,13 +5,17 @@
         static void Main(string[] args)
         {
             const int GRID_SIZE = 3;
-            string userInput;
+            const int ADJUSTING_INDEX = 1;
+            int userInputCollum;
+            int userInputRow;
             string[,] grid = new string[GRID_SIZE, GRID_SIZE];
             while (true)
             {
-                UIMethods.Write("Place your symbol.");
-                userInput = UIMethods.ReadLine();
-                UIMethods.Write(userInput);
+                UIMethods.Write("To place your symbol first choose a collum. (1/2/3)\n");
+                userInputCollum = Convert.ToInt32(UIMethods.ReadLine());
+                UIMethods.Write("To place your symbol first choose a row. (1/2/3)\n");
+                userInputRow = Convert.ToInt32(UIMethods.ReadLine());
+                grid[userInputCollum-ADJUSTING_INDEX, userInputRow-ADJUSTING_INDEX] = " X ";
                 UIMethods.Write("\n");
 
                 Logic.CheckGrid(grid,GRID_SIZE);
